f484d0fb64788a4c3e807ae2cb0834c834a62f71
HuyenTran02 <ngngochuyentran2002@gmail.com>
2023-09-13 11:42:30 +0700

    BTL

--- QuanLyThuVien/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.vttanh.quanlythuvien {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-    requires java.sql;
-    opens com.vttanh.quanlythuvien to javafx.fxml;
-    exports com.vttanh.quanlythuvien;
-    exports com.vttanh.pojo;
-}
